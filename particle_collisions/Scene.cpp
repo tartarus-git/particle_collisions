520ab1b35b450522957b66bd36f45059ad2b43c0
tartarus-git <niktedig@gmail.com>
2022-02-13 17:23:45 +0100

    Changed up the math in the Scene class after redoing it, I'm not sure if the math I already had was equivalent to the math now, I'll check the commits later and see what I did wrong (if anything) in the original version. Doesn't work yet for a couple of reasons, one of which is a major design flaw that is preventing the system from calculating physics properly, but it should be easy enough to fix so we'll hopefully have a stable system soon. Also added a huge wall of text because I researched something and wanted to take notes, it has almost nothing to do with the codebase.

--- particle_collisions/Scene.cpp
+++ particle_collisions/Scene.cpp
@@ -1,5 +1,7 @@
 #include "Scene.h"
 
+#include "debugOutput.h"
+
 void Scene::setupParticleHelpers() {
 	lastParticleCollisions.resize(particleCount);
 	for (int i = 0; i < particleCount; i++) {
@@ -28,16 +30,16 @@ void Scene::findCollision(size_t aIndex, size_t bIndex) {
 	// The main reason this is here is for protection against floating point rounding error:
 	// If collision is tested with a particle that is now touching (thanks to collision resolution), due to rounding errors, that object will probably count as a collision.
 	// To avoid this, just don't check collisions with that object until this object has collided off of something else and the question can be asked again.
-	if (lastParticleCollisions[aIndex] == bIndex) { return; }
+	if (lastParticleCollisions[aIndex] == bIndex) { return; }			// TODO: How does this issue get resolved. They've already collided, but if we leave this like this, the collider will collide them again, thats bad. Fix this whole system, the structure is terrible.
 
 	Particle& alphaRef = particles[aIndex];
 	Particle& betaRef = particles[bIndex];
 	Particle alpha = alphaRef;			// TODO: See if you should do this or not. Potentially compare assembly. Ask on stackoverflow as well maybe.
-	alpha.vel *= 1 - stepProgress;			// TODO: This is a quick fix, doesn't look good.
+	alpha.vel *= 1 - prevStepProgress;			// TODO: This is a quick fix, doesn't look good.
 	Particle beta = betaRef;
-	beta.vel *= 1 - stepProgress;
+	beta.vel *= 1 - prevStepProgress;
 
-	Vector2f posDiff = alpha.pos - beta.pos;
+	/*Vector2f posDiff = alpha.pos - beta.pos;
 	float minDist = alpha.radius + beta.radius;
 	float squareMinDist = minDist * minDist;
 	Vector2f specialVelDiff = alpha.vel - 2 * beta.vel;
@@ -51,29 +53,26 @@ void Scene::findCollision(size_t aIndex, size_t bIndex) {
 			alphaRef.pos -= posDiff;
 		}
 		return;
-	}
+	}*/
 
 	// a coefficient
-	float a = (alpha.vel * specialVelDiff + beta.vel * beta.vel).sum();
+	float a = (alpha.vel - beta.vel) % (alpha.vel - beta.vel);
 
 	// b coefficient
-	float b = (2 * posDiff * (alpha.vel - beta.vel)).sum();
+	float b = (alpha.vel + beta.vel) % (alpha.pos - beta.pos) * 2;
 
 	// c coefficient
-	float c = (alpha.pos * (alpha.pos - 2 * beta.pos) + beta.pos * beta.pos).sum();
-	// make one side equal to zero
-	c -= squareMinDist;
+	float c = (alpha.pos % alpha.pos) + (beta.pos % beta.pos) - (alpha.radius + beta.radius) * (alpha.radius + beta.radius);
 
-	// Reduce a coefficient to 1.
-	b /= 2 * a;				// Simultaniously construct sudo p term.
-	c /= a;
-
-	// Construct intra-root term.
-	float r = b * b - c;
+	// Construct determinant.
+	float r = b * b - 4 * a * c;
 
 	// If no collisions (because parallel trajectories that are too far away from each other for a parallel collision), return.
 	// We don't put 1 into earliestCollisions list here because the default value in earliestCollisions is 1. We don't have to do anything.
-	if (r < 0) { return; }
+	if (r < 0) {
+			lastParticleCollisions[aIndex] = aIndex;
+			lastParticleCollisions[bIndex] = bIndex;
+		return; }
 
 	// Following gets triggered if 1 collision (parallel lines that are exactly the right distance away), or 2 collisions (all other non-handled collisions).
 	// Inefficient to have separate branch for 1 collision because it happens so rarely, just handle it through the math of 2 collision handler.
@@ -83,33 +82,28 @@ void Scene::findCollision(size_t aIndex, size_t bIndex) {
 	float t2 = b - r;
 	float actualT;			// Find the actual collision.
 	if (t1 > 1 || t1 <= 0) {				// TODO: How does nan compare with normal numbers? Does it always return a false comparison?
-		if (t2 > 1 || t2 <= 0) { return; }							// Both solutions are out of bounds. No collision.
+		if (t2 > 1 || t2 <= 0) {
+			lastParticleCollisions[aIndex] = aIndex;
+			lastParticleCollisions[bIndex] = bIndex;
+			return; }							// Both solutions are out of bounds. No collision.
 		actualT = t2;												// x2 is the correct solution.
 	} else if (t2 > 1 || t2 <= 0) { actualT = t1; }					// x1 is the correct solution.
 	else { if (t1 < t2) { actualT = t1; } else { actualT = t2; } }	// The lower of the two solutions if the correct solution. This is because the collision that happens the earliest is the real one.
 	
-	if (actualT < stepProgress) { stepProgress += (1 - stepProgress) * actualT; }
+	if (actualT < stepProgress) { stepProgress = actualT; }
 	finished = false;				// Make sure everybody knows that this substep still had a collision in it. TODO: See if you can somehow do something smart with stepProgress to signal the same thing without needing to set a bool every time here.
-
-			/*Vector2f normal = (other.pos - pos).normalize();
-			Vector2f relV = ((vel * normal) * normal) - ((other.vel * normal) * normal);
-			other.vel += relV;
-			vel -= relV;
-
-			collisionFlag = true;
-
-
-			debuglogger::out << "collision detected!" << debuglogger::endl;
-			return;*/
+	lastParticleCollisions[aIndex] = bIndex;
+	lastParticleCollisions[bIndex] = bIndex;
 	return;
 }
 
 void Scene::resolveCollisions() {
 	for (int i = 0; i < particleCount; i++) {
+		if (lastParticleCollisions[i] == i) { continue; }			// If nothing to collide with for this particle or if this particle's collision is recorded in another particles lastParticleCollision, do nothing.
 		Particle& particle = particles[i];
 		particle.pos += particle.vel * stepProgress;
-		if (lastParticleCollisions[i] == i) { continue; }			// If nothing to collide with for this particle or if this particle's collision is recorded in another particles lastParticleCollision, do nothing.
 		Particle& other = particles[lastParticleCollisions[i]];				// TODO: You gotta move the other particle in this function as well or else this ain't gonna work.
+		other.pos += other.vel * stepProgress;
 		Vector2f normal = (other.pos - particle.pos).normalize();
 		Vector2f relV = ((particle.vel % normal) * normal) - ((other.vel % normal) * normal);
 		particle.vel -= relV;
@@ -119,12 +113,15 @@ void Scene::resolveCollisions() {
 
 void Scene::step() {
 	while (true) {
+		prevStepProgress = stepProgress;
+		stepProgress = 1;
 		// Find the earliest safepoint of all the particles.
 		for (int i = 0; i < lastParticle; i++) {
 			for (int j = i + 1; j < particleCount; j++) {				// TODO: For loop does first iteration before checking right? If it doesn't that is unnecessary work here.
 				findCollision(i, j);
 			}
 		}
+		stepProgress = prevStepProgress + (1 - prevStepProgress) * stepProgress;
 		if (finished) { break; }
 		finished = true;			// Setup for next round. TODO: You should stop setting this in class global and always set it above first for loop.
 
--- particle_collisions/main.cpp
+++ particle_collisions/main.cpp
@@ -33,6 +33,45 @@
 // Because of all this parallelism and decision making (it's very much akin to a massively complicated program which has been translated to hardware), the CPU is potentially more accurately visualized as a stream of water that gets split up into many smaller streams that each go their own routes and maybe combine back up at the end,
 // instead of a rigidly serial process.
 
+// SIDE-NOTE THAT I'M WRITING DOWN TO REMEMBER IT IN FUTURE, HAS VERY LITTLE TO DO WITH THIS CODEBASE:
+// ELF sections (or segments, I'm not sure, although someone online said that the two words have slightly different meanings and cannot be used interchangeably, so you still have to figure that out (TODO)):
+// .text --> code is stored here (executable, obviously, but also non-writeable for security reasons (you can use a few syscalls to change the permissions of the different sections though and make the text portion writeable (useful for self-modifying code))).
+// .data --> storage for global and/or static variables, because those have a default value that the OS can just write into your virtual memory before your program starts (default value specified by setting them equal to something in C++ (int something = 1 in global scope for example)).
+//						data section has read/write access since the variables can be changed at runtime. AFAIK, it's up to the compiler if it wants to put your values in the data section, it could also just lazy initialize them when they are used for the first time, but that's more expensive for the application and a really dumb
+//						thing to do so I don't know why the compiler would do that. It's different for static variables inside functions though, because those could depend on values that are not known at compile-time, eliminating the possibility of using the data section to initialize. Here, lazy initialization is garanteed by the standard.
+// .bss -->	AFAIK, this one isn't an actual section full of data in the ELF file, it's probably just a span that specifies from which address to which other address zeros should be written, this is done as an optimization so that global/static variables that are 0 don't have to be unnecessarily written to data section and don't have to
+//						make the ELF bigger. The OS zeros everything specified by the .bss section out and then starts your program.
+// .rodata --> This is read-only data, used for string literal storage and (I assume) for global const variables. It's sometimes combined with .text as an optimization so that the OS has less to worry about, but that's not super important.
+//						If you want string literals to be in the .data section instead of the .rodata section, because you want to change them at runtime for example, you can do char[] = "abc", which is the same thing as char[] = { 'a', 'b', 'c', '\0' }. This language feature was put in to (I assume) make it easier to initialize char arrays.
+//						I'm not totally sure that the string literal won't also exist in the .rodata section, but why would it, the compiler wouldn't be so stupid as to do such unnecessary stuff, so the char[] method is probably exactly what you would be looking for.
+// .tdata and .tbss --> thread local versions of bss and data, which are (I assume) loaded once on program startup, so you have access to the variables, but again everytime you create a new thread (so that the thread has access to it's own set of the variables, each initialized to the default value specified in .tdata).
+// What is thread local? Thread local variables are basically like global variables, but they don't get carried across threads (threads have different stacks, but everything else, including .data segment, where the global variables reside, is shared between them (obviously tdata and tbss are not shared, thats the whole point)).
+//				So global variables can be changed from all currently active threads of a process, but thread local variables (you can create them with the thread_local attribute on global variables), are copied for each thread so that each thread has it's own copy of the variables.
+//				The copying is done by the runtime, but I'm very much not an expert on that, still confused about how exactly that gets accomplished.
+// ANOTHER SUPER IMPORTANT THING: When a new thread is created, like I've already said, it gets a new stack, but it has the same virtual address space as the other threads, which is due to the requirement of needing to be able to share pointers and access each others heap and stack memory, which is possible and really cool.
+//		The problem with that is that the stacks are in the same address space, so they create limits for each other (the stack is already limited by the maximum addressable size of virtual memory, which is determined by the bitwidth of the machine (32 or 64-bit machines), and also by the memory layout in virtual memory.
+//		it definitely can't expand forever, take a look at a typical memory layout for a process and then you'll understand better. I think a stack can have a max size of 80MB in most systems or something, but thats the 32-bit version so idk how it is on 64-bit machines.)
+//		Creating threads adds more stacks, which (at least on 32-bit systems), presumably causes some memory issues. It seems probable that the maximum stack size goes down when you allocate new thread stack storage and such, which is super interesting, but shouldn't ever be a problem since heavy duty allocations should be
+//		done in the heap anyway, which is shared between threads so I guess congestion becomes less of a problem, although if you need to allocate a bunch of copies of the same huge array for each thread in the heap, obviously you can still encounter the edge of virtual memory or whatever other bounds you've been given by your
+//		memory layout.
+// ANOTHER INTERESTING THING: Once a CPU gets put into virtual addressing mode, it applies for all processes, even the kernel, which means that, when you do a syscall and the system needs to switch into the kernels context after doing the interrupt (although interrupts are not the only way to implement syscalls), the virtual addresses
+// need to be remapped so the kernel can use it's virtual addresses to address the appropriate physical memory. This is apparently a rather expensive operation, which is why it is avoided as much as possible (in the context of switching between regular programs that is). To avoid this when switching to kernel memory, which presumably
+// happens way more often than regular task switches because everyones probably doing syscalls all the time, kernel memory's virtual address space is set to be present in every programs address space and is just made inaccessible to every program. In a 3/1 distribution, a programs virtual address space is divided so that
+// the program has access to 3/4 of it and the kernel has access to the last 1/4, which in the context of 32-bit systems means that the program has 3 GB and the kernel has 1GB. For 64-bit systems the division is often half-half, but I'm haven't researched that all that much. That allows the kernel space of virtual memory to be
+// constantly mapped to the kernels physical memory, and when your doing a syscall or something, no expensive virtual memory switching is required, the context is just switched from user to kernel mode and then the machine just starts doing things with the kernel memory in your virtual memory.
+// The virtual kernel memory is also mmapped so that everyones kernel memory sections stay synchronized I believe. This isn't super hard because by mapping the virtual memory to the same physical memory, you've got this behaviour for free, which is how mmap works.
+// This is done so that the kernel can maintain it's state regardless of which processes virtual memory it is currently residing in, at least that would make sense to me.
+
+// ANOTHER THING: I've mentioned this a couple times, but I'll do it again, it's possible to create static variables in functions. I didn't know this before, and I think it's a weird feature for C++ to have, since I don't really know when I would ever use it, but it's possible.
+// The behaviour is such that the variable gets initialized the first time the initialization code is processed in the function. One main difference, is that the lifetime of the variable is the lifetime of the program. It doesn't get destroyed when the function returns, it gets destroyed when the program exits.
+// The second main difference is that you can call the function twice but the variable only gets initialized once. Static function variables are lazy initialized, so the first initialization is when the static variable gets initialized, and every time you call the function again after that and the initialization code is passed,
+// it does nothing. AFAIK, it doesn't even update the value in case the values that are used to initialize have changed since the first initialization, it just straight up leaves it alone for the rest of the program. It's not const though, so you can still change it as you please manually.
+// Since this all heavily depends on the runtime behaviour of your program and what branches the program takes and when the function with the static variable gets called, the compiler has to put in a little bit of code that makes sure that you never initialize the static variable twice. This is probably done through a simple
+// boolean, but it is still technically an inefficiency. By moving the static variable outside the scope of the function and making it global, you avoid this extra runtime penalty (only as long as C++ doesn't do lazy initialization on global variables, I don't think so because I cant think of a situation where C++ would let you
+// initialize a global variable with something that isn't known at compile-time, if it does, then lazy initialization is still an issue and if you don't want it, you have to declare the variable at global scope and then initialize it somewhere specific (default constructor might get in your way there), TODO: make this thing more precise by looking this stuff up and removing some of your guess-work from this comment). If you still want to
+// initialize the variable with something that isn't known at compile-time, which is possible with static variables but (probably) not with global variables, then you have to just create the global variable, but then use a move assignment operator with a temporary object constructed with some sort of constructor to set the global variable equal
+// to whatever value you want it be somewhere in your code. You've basically taken the lazy initialization out of the runtimes hands and hardcoded a spot where it's done, which avoids the overhead of normal lazy initialization.
+
 unsigned int windowWidth;
 unsigned int windowHeight;
 
@@ -72,7 +111,7 @@ void graphicsLoop() {			// TODO: Just expose this g stuff in the library so we d
 
 	std::vector<Particle> particles;
 	for (int i = 0; i < 2; i++) {
-		particles.push_back(Particle(Vector2f(rand() % windowWidth, rand() % windowHeight), Vector2f(0, 0), 20, 1));
+		particles.push_back(Particle(Vector2f(rand() % windowWidth, rand() % windowHeight), Vector2f(1, 0), 20, 1));
 	}
 	Scene scene(particles);
 
@@ -86,6 +125,6 @@ void graphicsLoop() {			// TODO: Just expose this g stuff in the library so we d
 		SelectObject(g, particleBrush);
 		renderer.render(scene);
 		BitBlt(finalG, 0, 0, windowWidth, windowHeight, g, 0, 0, SRCCOPY);
-		//scene.step();
+		scene.step();
 	}
 }
